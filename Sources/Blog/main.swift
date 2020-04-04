cb8ca7d8220a0dbaecb765b7b3a2639c14523f8c
phucld <phucld@dwarvesv.com>
2020-04-04 23:35:46 +0700

    Init project

---
+++ Sources/Blog/main.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct Blog: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+        case about
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+        
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://phucledien.github.io")!
+    var name = "Phuc Le Dien"
+    var description = "A description of Blog"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try Blog().publish(withTheme: .foundation)
